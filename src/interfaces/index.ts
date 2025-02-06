1101536eb1e3515452ef0f093a7ae08b855efff1
mmasser95 <mmasser95@gmail.com>
2025-02-06 13:28:14 +0100

    TASK: Interface to retrieve homePage content

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export default interface Index {
+    id: number;
+    content: string;
+    image: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    locale: string
+}
